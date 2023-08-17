daa4c7a262008184a72bb3ead216dd2cca62cceb
Antonio Pitasi <antonio@pitasi.dev>
2023-08-17 12:11:51 +0200

    github copilot

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,23 +1,27 @@
 return {
 	{
-		"zbirenbaum/copilot.lua",
+		"github/copilot.vim",
 		cmd = "Copilot",
 		event = "InsertEnter",
 		config = function()
-			require("copilot").setup({
-				suggestion = {
-					auto_trigger = true,
-					keymap = {
-						accept = "<C-l>",
-						accept_word = false,
-						accept_line = false,
-						next = "<C-.>",
-						prev = "<C-,>",
-						dismiss = "<C/>",
-					},
-				},
-				panel = { enabled = true },
-			})
+			-- imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+			vim.g.copilot_no_tab_map = true
+			vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
+			-- require("copilot").setup({
+			-- 	suggestion = {
+			-- 		auto_trigger = true,
+			-- 		keymap = {
+			-- 			accept = "<C-l>",
+			-- 			accept_word = false,
+			-- 			accept_line = false,
+			-- 			next = "<C-.>",
+			-- 			prev = "<C-,>",
+			-- 			dismiss = "<C/>",
+			-- 		},
+			-- 	},
+			-- 	panel = { enabled = true },
+			-- })
 		end,
 	},
 }
